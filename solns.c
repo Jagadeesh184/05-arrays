701160b497b7739c5ef31e7ee9fb675bb77b6ac8
Jagadeesh184 <85110883+Jagadeesh184@users.noreply.github.com>
2021-06-19 22:51:03 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,29 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int arr[], int n)
+{
+  int i;
+  int max=arr[0];
+  for(i=1;i<n;i++)
+    if(arr[i]>max)
+      max=arr[i];
+  return max;
+  int min (int arr[],int n)
+  {
+    int i;
+    int min=arr[0];
+    for(i=1;i<n;i++)
+      if(arr[i]<min)
+        min=arr[i];
+    return min;
+  }
+  float average(int arr[],int n);
+  total=total+=arr[i];
+  average=total\3.0;   
+}
+int main(void){
+  int arr[]={10,20,30};
+  int n=size of(arr)/size of arr[0]);
+  printf("max in given array is %i,min in given array is %i,%f\n", max(arr,n),min(arr,n) average};
+         
+         }
